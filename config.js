eb3fe642c124db15ec407623c8d269ac350dfd88
kiowoyo21-eng <kiowoyo21@gmail.com>
2026-08-19 15:59:36 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,11 @@
  * paste its /exec URL here.
  */
 const CRM_CONFIG = {
+
   API_URL:
-    "https://script.google.com/macros/s/AKfycbwDmJGpj37YWOvP5gGbw-ARe1WuZG3H1cOYWVByhPnVjFv4dXxRXu8UFR2yQ1xVUIBJ/exec",
+    "/api/crm",
 
   APP_NAME:
     "CRM Portal"
+
 };
